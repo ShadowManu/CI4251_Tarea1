d6808da2e5d0bb9817130e666f4e0c1cfd3ec04f
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-04-29 22:27:34 -0430

    add image generation to main function

--- Main.hs
+++ Main.hs
@@ -10,6 +10,9 @@ Example main to explain and verify the solution functions
 -}
 module Main (main) where
 
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+
 import Types (Sample(Sample), x, y)
 import Constants (alpha, guess)
 import Samples (training)
@@ -24,12 +27,21 @@ main = do
   mapM_ print . take 3 $ gd alpha guess training
   putStr "\n"
 
-  let result = last $ gd alpha guess training
-  let (_, vector, _) = result
+  let
+    results = gd alpha guess training
+    lastResult = last results
+    (_, vector, _) = lastResult
 
   putStrLn "Imprimiendo el ultimo resultado..."
-  print result
+  print lastResult
   putStr "\n"
 
   print $ "El valor v associado al vector " ++ show (x sample) ++ " es " ++ show (theta vector sample)
   putStr "\n"
+
+  -- Code to generate the image file
+  -- can be removed to discard Graphics imports
+  toFile def "works.png" $ do
+    layout_title .= "Cost over iterations"
+    plot (line "am" . (:[]) . map (\(x,_,y) -> (x,y)) $ results)
+  return ()
